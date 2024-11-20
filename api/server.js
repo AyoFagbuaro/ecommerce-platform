07af461b43bd3edada5831a6de2984180b823375
Ropo Fagbuaro <fagbuaf@Ropos-MacBook-Pro.local>
2024-11-20 00:22:39 +0000

    cleaning up final

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 // Simple endpoint to fetch data
 app.get('/api/data', (req, res) => {
